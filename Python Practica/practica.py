101a0e452752a84e53b77a80f9136f8ffa22a7cb
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-04-25 15:59:21 -0500

    Python

---
+++ Python Practica/practica.py
@@ -0,0 +1,28 @@
+
+"""
+
+def opcion():
+    opcion = int(input("Ingrese 1 para suma y 2 para resta: "))
+
+    if opcion == 1:
+        print("Suma")
+
+    elif opcion == 2:
+        print("Resta")
+
+    else:
+        print("Aprende a leer neardental")
+
+"""
+
+
+opcion = int(input("Ingrese 1 para suma y 2 para resta: "))
+if opcion == 1: 
+    print("suma")
+
+elif opcion == 2:
+    print("resta")
+
+else:
+    print("Aprende a leer neardental")
+
