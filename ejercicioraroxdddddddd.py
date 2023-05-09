72f25f7daf8d3bac6d2a6fde4b42e78f73435dc3
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-09 09:39:14 -0500

    update

---
+++ ejercicioraroxdddddddd.py
@@ -0,0 +1,34 @@
+
+"""
+color = input("Selecciona un color: A o a para azul, R o r para rojo: ")
+
+if color == "A" or "a":
+    color2 = input("Selecciona el segundo color: V o v para verde, AM o am para amarillo: ")
+    if color2 != "V" or "v":
+        print("La combinación de los colores será azul verdoso")
+    elif color2 != "AM" or "am":
+        print("La combinación de los colores será verde")
+else:
+    print("Imprima un color valido")
+
+if color == "R" or "r":
+    color2 = input("Selecciona el segundo color: V o v para verde, AM o am para amarillo: ")
+    if color2 != "V" or "v":
+        print("La combinación de los colores será marrón")
+    elif color2 != "AM" or "am":
+        print("La combinación de los colores será morado")
+else:
+    print("Imprima un color valido")
+
+edad = int(input("Digite su edad: "))
+
+if edad > 0 <= 18:
+    print("No puede salir")
+elif edad >= 18 <= 31:
+    print("Puede salir dos horas")
+elif edad > 31:
+    print("Puede salir xd")
+else:
+    print("error")
+"""    
+
