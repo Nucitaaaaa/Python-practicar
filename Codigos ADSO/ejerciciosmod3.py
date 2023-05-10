0b2022a9f99f53701a25705b1603115fbdc86e88
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-10 15:36:57 -0500

    Delete and changes

--- Codigos ADSO/ejerciciosmod3.py
+++ Codigos ADSO/ejerciciosmod3.py
@@ -8,8 +8,6 @@ ramdom = int(input("Digita cualquier numero: "))
 if ramdom > 100000:
     print("Un numero gigante")
 
-
-
 #if_else
 
 numero = int(input("Digita tu edad: "))
@@ -92,3 +90,75 @@ num = 0
 
 for i in range (15):
     print(i)
+
+#Ejercicio_sebastian
+
+print("Bienvenido, este programa tiene el fin de mostrarle algunas combinaciones de colores\n")
+
+color = str(input("Ingrese el color inicial, ingrese A para elegir Azul, o R para elegir Rojo: "))
+color2 = str(input("Ingrese el segundo color a mezclar, ingrese V para elegir Verde, o AM para elegir Amarillo: "))
+
+if color == "A" and color2 == "V":
+    print("La combinación de estos colores dará como resultado el color Azul Verdoso")
+elif color == "A" and color2 == "AM":
+    print("La combinación de estos colores dará como resultado el color Verde")
+elif color == "R" and color2 == "V":
+    print("La combinación de estos colores dará como resultado el color Marrón")
+elif color == "R" and color2 == "AM":
+    print("La combinación de estos colores dará como resultado el color Naranja")
+else:
+    print("seleccione una combinación valida")
+
+
+#Ejercicio_edad
+
+edad = int(input("Digite su edad: "))
+
+if edad > 0 and edad < 17:
+    print("No puede salir")
+elif edad > 17 and edad < 29:
+    print("Puede salir dos horas")
+elif edad > 29:
+    print("Puede salir y volver cuando quiera")
+else:
+    print("Digite una edad correcta")
+
+
+#bucles
+
+for i in range(1,31):
+    if i % 2 != 0:
+        print(i)
+
+i = 2
+
+while i < 32:
+    print(i)
+    i += 2
+
+
+
+for i in range(1,21):
+    print("El numero es", i)
+    i += 1
+print("La suma de todos será:", i*10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
