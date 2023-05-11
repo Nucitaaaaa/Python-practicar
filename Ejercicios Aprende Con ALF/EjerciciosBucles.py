b85a3a14f72905e27b9924aaf15c8c94a627347a
SL234 <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-11 10:07:05 -0500

    Update EjerciciosBucles.py

--- Ejercicios Aprende Con ALF/EjerciciosBucles.py
+++ Ejercicios Aprende Con ALF/EjerciciosBucles.py
@@ -6,6 +6,20 @@ palabra = input("Ingrese una palabra: ")
 for i in range(0,10):
     print(palabra)
 
-"""
 
 #Ejercicio_2
+
+edad = int(input("Digite su edad: "))
+
+for i in range(0,edad + 1):
+    print(f"usted ha cumplido {i} años")
+
+"""
+
+#Ejercicio_3
+
+num = int(input("Digite un numero entero: "))
+
+for i in range(1,num):
+    if num % 2 != 0:
+        print(num)
