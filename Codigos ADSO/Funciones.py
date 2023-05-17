ae1fa0de0ab67f9c3f80d1dc296a1848e65e3789
Nucitaaaaa <123673043+Nucitaaaaa@users.noreply.github.com>
2023-05-17 15:05:06 -0500

    Update Funciones.py

--- Codigos ADSO/Funciones.py
+++ Codigos ADSO/Funciones.py
@@ -103,6 +103,37 @@ xd (9,5,7)
 
 
 
+try:
+    value = input("Enter a value: ")
+    print(value/value)
+except ValueError:
+    print("Bad input...")
+except ZeroDivisionError:
+    print("Very bad input...")
+except TypeError:
+    print("Very very bad input...")
+except:
+    print("Booo!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
